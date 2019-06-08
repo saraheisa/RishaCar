ebd50d2733e02ac455b061ca397639f6ee050762
Sara Eisa <prosarahgamal@outlook.com>
2019-06-08 23:48:27 +0200

    edit users

--- handlers/users.py
+++ handlers/users.py
@@ -4,6 +4,7 @@ from bson import json_util
 from bson.json_util import dumps
 from handlers.base import BaseHandler
 from lib.DBConnection import UserFunctions
+from cryptography.fernet import Fernet
 
 import logging
 logger = logging.getLogger('rishacar.' + __name__)
@@ -11,42 +12,102 @@ logger = logging.getLogger('rishacar.' + __name__)
 @jwtauth
 class UsersHandler(BaseHandler):
   async def get(self, id):
+    # get other users' data
     if id:
       userFunc = UserFunctions()
-      result = await userFunc.getUser(id = id)
+      result = await userFunc.getUser(id=id)
       if result:
+        result['_id'] = str(result['_id'])
+        del result['password']
         self.write(json_util.dumps(result))
       else:
         self.set_status(500)
         self.write({"message":"database error"})
         self.finish()
+    # get the user's data
     else:
-      self.set_status(400)
-      self.write({"message":"missing id"})
-      self.finish()
-  
-  async def put(self, id):
-    if id:
       if self.request.body:
         data = json.loads(self.request.body)
-        userFunc = UserFunctions()
-        result = await userFunc.updateUser(id, data)
-        self.write({"message":"user updated"})
+        if data['id']:
+          id = data['id']
+          userFunc = UserFunctions()
+          result = await userFunc.getUser(id=id)
+          if result:
+            result['_id'] = str(result['_id'])
+            del result['password']
+            self.write(json_util.dumps(result))
+          else:
+            self.set_status(500)
+            self.write({"message":"database error"})
+            self.finish()
+        else:
+          self.set_status(400)
+          self.write({"message":"missing id"})
+          self.finish()
       else:
         self.set_status(400)
-        self.write({"message":"missing data"})
+        self.write({"message":"missing id"})
+        self.finish()
+  
+  async def put(self, id):
+    if self.request.body:
+      data = json.loads(self.request.body)
+      if data['id']:
+        id = data['id']
+        userFunc = UserFunctions()
+        del data['id']
+        result = await userFunc.updateUser(id, data)
+        if result:
+          self.write({"message":"user updated"})
+        else:
+          self.set_status(500)
+          self.write({"message":"database error"})
+          self.finish()
+      else:
+        self.set_status(400)
+        self.write({"message":"missing id"})
         self.finish()
     else:
       self.set_status(400)
-      self.write({"message":"missing id"})
+      self.write({"message":"missing data"})
       self.finish()
 
   async def delete(self, id):
-    if id:
-      userFunc = UserFunctions()
-      result = await userFunc.deleteUser(id)
-      self.write({"message":"user deleted"})
+    if self.request.body:
+      data = json.loads(self.request.body)
+      if 'id' in data:
+        if 'password' in data:
+          id = data['id']
+          password = data['password']
+          userFunc = UserFunctions()
+          user = await userFunc.getUser(id=id)
+          key = b'i102LDEGa-8PLuZJ9kw-VR2VKCeYxOanZvM4KQAZLt8='
+          user['password'] = self.decrypt(key, bytes(user['password'], 'utf-8')).decode("utf-8")
+          if user['password'] == data['password']:
+            result = await userFunc.deleteUser(id)
+            if result:
+              self.write({"message":"user deleted"})
+            else:
+              self.set_status(500)
+              self.write({"message":"database error"})
+              self.finish()
+          else:
+            self.set_status(400)
+            self.write({"message":"wrong password"})
+            self.finish()
+        else:
+          self.set_status(400)
+          self.write({"message":"missing password"})
+          self.finish()
+      else:
+        self.set_status(400)
+        self.write({"message":"missing id"})
+        self.finish()
     else:
       self.set_status(400)
-      self.write({"message":"missing id"})
+      self.write({"message":"missing data"})
       self.finish()
+
+  def decrypt(self, key, text):
+    cipher_suite = Fernet(key)
+    return (cipher_suite.decrypt(text))
