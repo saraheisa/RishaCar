d0d525c1323826087d95cef9c74a779f273bacb6
Sara Eisa <prosarahgamal@outlook.com>
2019-06-08 23:45:36 +0200

    edit login

--- handlers/login.py
+++ handlers/login.py
@@ -1,9 +1,7 @@
 import json
 import base64
 import logging
-# from Crypto.Cipher import AES
-# from Crypto.Hash import SHA256
-# from Crypto import Random
+from cryptography.fernet import Fernet
 from bson import json_util
 from bson.json_util import dumps
 from handlers.base import BaseHandler
@@ -11,11 +9,11 @@ from lib.DBConnection import UserFunctions
 
 logger = logging.getLogger('rishacar.' + __name__)
 
+
 class LoginHandler(BaseHandler):
   async def post(self):
     if self.request.body:
       data = json.loads(self.request.body)
-      # print(encrypt(""))
       user = ''
       if ('password' in data) & ('username' in data):
         userFunc = UserFunctions()
@@ -24,10 +22,12 @@ class LoginHandler(BaseHandler):
         userFunc = UserFunctions()
         user = await userFunc.getUser(email=data['email'])
       if user:
+        key = b'i102LDEGa-8PLuZJ9kw-VR2VKCeYxOanZvM4KQAZLt8='
+        user['password'] = self.decrypt(key, bytes(user['password'], 'utf-8')).decode("utf-8")
         if user['password'] == data['password']:
           token = super(LoginHandler, self).jwtEncode()
           self.encoded = token
-          response = {'token': self.encoded.decode('ascii')}
+          response = {'token': self.encoded.decode('ascii'), 'id': str(user['_id'])}
           self.write(response)
         else:
           self.set_status(400)
@@ -42,23 +42,6 @@ class LoginHandler(BaseHandler):
       self.write({"message":"data is missing"})
       self.finish()
   
-  # def encrypt(self, key, source, encode=True):
-  #   key = SHA256.new(key).digest()  # use SHA-256 over our key to get a proper-sized AES key
-  #   IV = Random.new().read(AES.block_size)  # generate IV
-  #   encryptor = AES.new(key, AES.MODE_CBC, IV)
-  #   padding = AES.block_size - len(source) % AES.block_size  # calculate needed padding
-  #   source += bytes([padding]) * padding  # Python 2.x: source += chr(padding) * padding
-  #   data = IV + encryptor.encrypt(source)  # store the IV at the beginning and encrypt
-  #   return base64.b64encode(data).decode("latin-1") if encode else data
-
-  # def decrypt(self, key, source, decode=True):
-  #   if decode:
-  #       source = base64.b64decode(source.encode("latin-1"))
-  #   key = SHA256.new(key).digest()  # use SHA-256 over our key to get a proper-sized AES key
-  #   IV = source[:AES.block_size]  # extract the IV from the beginning
-  #   decryptor = AES.new(key, AES.MODE_CBC, IV)
-  #   data = decryptor.decrypt(source[AES.block_size:])  # decrypt
-  #   padding = data[-1]  # pick the padding value from the end; Python 2.x: ord(data[-1])
-  #   if data[-padding:] != bytes([padding]) * padding:  # Python 2.x: chr(padding) * padding
-  #     raise ValueError("Invalid padding...")
-  #   return data[:-padding]  # remove the padding
+  def decrypt(self, key, text):
+    cipher_suite = Fernet(key)
+    return (cipher_suite.decrypt(text))
